386074fabf0e444a78fd361abde47ea72b9ab7ba
devpolas <195308698+devpolas@users.noreply.github.com>
2026-07-05 20:11:55 +0600

    fix animation issue

--- components/sections/contact/contact.tsx
+++ components/sections/contact/contact.tsx
@@ -148,8 +148,8 @@ export default function Contact() {
           {/* Contact Form */}
 
           <motion.div
-            initial={{ opacity: 0, x: 40 }}
-            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6 }}
             className="lg:col-span-3"
           >
