d448f54702a69c13d27fe91cbb29ec12af9c3900
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2026-07-27 23:15:12 +0600

    fix bug

--- components/sections/hero/hero-intro.tsx
+++ components/sections/hero/hero-intro.tsx
@@ -1,18 +1,18 @@
 export default function HeroIntro() {
   return (
     <div className="space-y-4">
-      <h1 className="text-4xl leading-tight font-bold md:text-6xl">
-        Hi, I&apos;m
-        <span className="bg-linear-to-r from-primary to-purple-500 bg-clip-text text-transparent">
+      <h1 className="font-bold text-4xl md:text-6xl leading-tight">
+        Hi, I&apos;m{" "}
+        <span className="bg-clip-text bg-linear-to-r from-primary to-purple-500 text-transparent">
           Polas
         </span>
       </h1>
 
-      <h2 className="text-2xl font-semibold text-muted-foreground md:text-4xl">
+      <h2 className="font-semibold text-muted-foreground text-2xl md:text-4xl">
         Full Stack Developer
       </h2>
 
-      <p className="max-w-xl text-lg leading-relaxed text-muted-foreground">
+      <p className="max-w-xl text-muted-foreground text-lg leading-relaxed">
         I&apos;m a passionate Full Stack Web Developer who enjoys building
         modern, scalable, and user-focused web applications. I love turning
         ideas into real-world products using React, Next.js, Node.js,
